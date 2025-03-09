f8aedbd5aefe96a08474ae3cce4029944d268803
camiurenda <urendacamila@gamil.com>
2025-03-09 12:57:36 -0300

    QUITO URL ALTERNATIVA

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pantera_db';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
